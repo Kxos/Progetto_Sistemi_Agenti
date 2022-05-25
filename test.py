289926b038db4e598bcd7971c0df0c5c8d3c8af0
Kxos <vito.iannone90@gmail.com>
2022-05-25 19:34:47 +0200

    Refactoring di test.py

--- test.py
+++ test.py
@@ -15,17 +15,18 @@ from torchvision import transforms
 from tqdm import tqdm
 
 from config import args, return_args
-from dataloader.opera7 import Opera7
+#from dataloader.opera7 import Opera7
 from dataloader.emovo import Emovo
 from dataloader.demos import Demos
 from dataloader.demosemovo import DemosEmovo
-from dataloader.demosemovogender import DemosEmovoGender
+from dataloader.affectnet import AffectNet
+#from dataloader.demosemovogender import DemosEmovoGender
 from models.bam.vggface2_bam import VGGFace2BAM
 from models.cbam.vggface2_cbam import VGGFace2CBAM
 from models.resnet50.vggface2 import VGGFace2
 from models.se.vggface2_se import VGGFace2SE
 from utility.checkpoint import load_model
-from utility.utility import setup_seed 
+from utility.utility import setup_seed
 from utility.confusion_matrix import show_confusion_matrix, get_classification_report
 
 warnings.filterwarnings('ignore')
@@ -34,16 +35,16 @@ setup_seed(args.seed)
 
 logger = logging.getLogger('mnist_AutoML')
 
-def main(args):
 
+def main(args):
     # Args for debugging through IDE
-    #args['dataset'] = 'demos'                                                          # Replace with you own dataset
-    #args['gender'] = 'all'                                                             # Gender for the training dataset
-    #args['validation'] = 'emotion'                                                     # Choose on what to do validation ("emotion" / "gender")
-    #args['uses_drive'] = False                                                         # Whether to choose Drive to save results
-    #args['loadModel'] = 'result/SysAg2022/best_model_gruppo_N_all.pt'                  # Load model for validation
-    #args['attention'] = 'bam'                                                          # Choose your model type (Bam) / (CBam)
-    #args['batch_size'] = 64                                                            # Batch size for training
+    # args['dataset'] = 'demos'                                                          # Replace with you own dataset
+    # args['gender'] = 'all'                                                             # Gender for the training dataset
+    # args['validation'] = 'emotion'                                                     # Choose on what to do validation ("emotion" / "gender")
+    # args['uses_drive'] = False                                                         # Whether to choose Drive to save results
+    # args['loadModel'] = 'result/SysAg2022/best_model_gruppo_N_all.pt'                  # Load model for validation
+    # args['attention'] = 'bam'                                                          # Choose your model type (Bam) / (CBam)
+    # args['batch_size'] = 64                                                            # Batch size for training
 
     print("Starting validation with the following configuration:")
     print("Attention module: {}".format(args['attention']))
@@ -57,28 +58,33 @@ def main(args):
     print("Uses Drive: {}".format(args["uses_drive"]))
     print("With Augmentation: {}".format(args["withAugmentation"]))
     print("Workers: {}".format(args['workers']))
-    
+
     if platform.system() == "Linux" and args['uses_drive']:
         print("----------------------------")
         print("** Google Drive Sign In **")
-        if not(os.path.exists("../../gdrive/")):
-            print("No Google Drive path detected! Please mount it before running this script or disable ""uses_drive"" flag!")
+        if not (os.path.exists("../../gdrive/")):
+            print(
+                "No Google Drive path detected! Please mount it before running this script or disable ""uses_drive"" flag!")
             print("----------------------------")
             exit(0)
         else:
             print("** Successfully logged in! **")
             print("----------------------------")
 
-        if not(os.path.exists("../../gdrive/MyDrive/SysAg2022/{}/{}/{}".format(args["dataset"], args["attention"], args["gender"]))):
-           os.makedirs("../../gdrive/MyDrive/SysAg2022/{}/{}/{}".format(args["dataset"], args["attention"], args["gender"]))
+        if not (os.path.exists(
+                "../../gdrive/MyDrive/SysAg2022/{}/{}/{}".format(args["dataset"], args["attention"], args["gender"]))):
+            os.makedirs(
+                "../../gdrive/MyDrive/SysAg2022/{}/{}/{}".format(args["dataset"], args["attention"], args["gender"]))
 
-        if not(os.path.exists("../../gdrive/MyDrive/SysAg2022/{}/{}/{}".format(args["dataset"], args["attention"], args["gender"]))):
-           os.makedirs("../../gdrive/MyDrive/SysAg2022/{}/{}/{}".format(args["dataset"], args["attention"], args["gender"]))
+        if not (os.path.exists(
+                "../../gdrive/MyDrive/SysAg2022/{}/{}/{}".format(args["dataset"], args["attention"], args["gender"]))):
+            os.makedirs(
+                "../../gdrive/MyDrive/SysAg2022/{}/{}/{}".format(args["dataset"], args["attention"], args["gender"]))
 
-    if not(os.path.exists(os.path.join("result", args["dataset"], args["attention"], args["gender"]))):
-           os.makedirs(os.path.join("result", args["dataset"], args["attention"], args["gender"]))
+    if not (os.path.exists(os.path.join("result", args["dataset"], args["attention"], args["gender"]))):
+        os.makedirs(os.path.join("result", args["dataset"], args["attention"], args["gender"]))
 
-    if(torch.cuda.is_available()):
+    if (torch.cuda.is_available()):
         device = torch.device("cuda")
         print("===================================================")
         print('Cuda available: {}'.format(torch.cuda.is_available()))
@@ -105,122 +111,37 @@ def main(args):
             transforms.Resize((224, 224)),
             transforms.ToTensor()
         ])
-    
-    if args["dataset"] == "opera7":
-        val_data = Opera7(gender=args["gender"], validation=args["validation"], split="val", transform=val_preprocess)
-        if (args["validation"] == "gender"):
-            classes = 2
-            label_mapping = {
-                0: "Donna",
-                1: "Uomo",
-            }
-        else:
-            classes = 7
-            label_mapping = {
-                0: "Disgusto",
-                1: "Gioia",
-                2: "Paura",
-                3: "Rabbia",
-                4: "Sorpresa",
-                5: "Tristezza",
-                6: "Neutrale"
-            }
-    elif args["dataset"]  == "emovo":
-        val_data = Emovo(gender=args["gender"], validation=args["validation"], split="val", transform=val_preprocess)
-        if (args["validation"] == "gender"):
-            classes = 2
-            label_mapping = {
-                0: "Donna",
-                1: "Uomo",
-            }
-        else:
-            classes = 7
-            label_mapping = {
-                0: "Disgusto",
-                1: "Gioia",
-                2: "Paura",
-                3: "Rabbia",
-                4: "Sorpresa",
-                5: "Tristezza",
-                6: "Neutrale"
-            }
-    elif args["dataset"]  == "demos":
-        val_data = Demos(gender=args["gender"], validation=args["validation"], split="val", transform=val_preprocess)
-        if (args["validation"] == "gender"):
-            classes = 2
-            label_mapping = {
-                0: "Donna",
-                1: "Uomo",
-            }
-        else:
-            classes = 7
-            label_mapping = {
-                0: "Disgusto",
-                1: "Gioia",
-                2: "Paura",
-                3: "Rabbia",
-                4: "Sorpresa",
-                5: "Tristezza",
-                6: "Neutrale"
-            }
-    elif args["dataset"]  == "demosemovo":
-        val_data = DemosEmovo(gender=args["gender"], validation=args["validation"], split="val", transform=val_preprocess)
-        if (args["validation"] == "gender"):
-            classes = 2
-            label_mapping = {
-                0: "Donna",
-                1: "Uomo",
-            }
-        else:
-            classes = 7
-            label_mapping = {
-                0: "Disgusto",
-                1: "Gioia",
-                2: "Paura",
-                3: "Rabbia",
-                4: "Sorpresa",
-                5: "Tristezza",
-                6: "Neutrale"
-            }
-    elif args["dataset"]  == "demosemovogender":
-        val_data = DemosEmovoGender(gender=args["gender"], validation=args["validation"], split="val", transform=val_preprocess)
-        if (args["validation"] == "gender"):
-            classes = 2
-            label_mapping = {
-                0: "Donna",
-                1: "Uomo",
-            }
-        else:
-            classes = 7
-            label_mapping = {
-                0: "Disgusto",
-                1: "Gioia",
-                2: "Paura",
-                3: "Rabbia",
-                4: "Sorpresa",
-                5: "Tristezza",
-                6: "Neutrale"
-            }
+
+    validation_classes = {
+        #"opera7": Opera7,
+        "emovo": Emovo,
+        "demos": Demos,
+        "demosemovo": DemosEmovo,
+        "affectnet": AffectNet,
+        #"demosemovogender": DemosEmovoGender
+    }
+
+    dataset = args["dataset"] if args["dataset"] in validation_classes.keys() else "affectnet"
+    val_data = validation_classes[dataset](gender=args["gender"], validation=args["validation"], split="val",
+                                           transform=val_preprocess)
+    if args["validation"] == "gender":
+        classes = 2
+        label_mapping = {
+            0: "Donna",
+            1: "Uomo",
+        }
     else:
-        val_data = Opera7(gender=args["gender"], split="val", validation=args["validation"], transform=val_preprocess)
-        if (args["validation"] == "gender"):
-            classes = 2
-            label_mapping = {
-                0: "Donna",
-                1: "Uomo",
-            }
-        else:
-            classes = 7
-            label_mapping = {
-                0: "Disgusto",
-                1: "Gioia",
-                2: "Paura",
-                3: "Rabbia",
-                4: "Sorpresa",
-                5: "Tristezza",
-                6: "Neutrale"
-            }
-    
+        classes = 7
+        label_mapping = {
+            0: "Disgusto",
+            1: "Gioia",
+            2: "Paura",
+            3: "Rabbia",
+            4: "Sorpresa",
+            5: "Tristezza",
+            6: "Neutrale"
+        }
+
     if args["attention"] == "no":
         model = VGGFace2(pretrained=False, classes=classes).to(device)
     elif args["attention"] == "se":
@@ -249,8 +170,9 @@ def main(args):
 
     # validate the model
     model.eval()
-    
-    val_loader = torch.utils.data.DataLoader(val_data, batch_size=args["batch_size"], shuffle=True, num_workers=args["workers"])
+
+    val_loader = torch.utils.data.DataLoader(val_data, batch_size=args["batch_size"], shuffle=True,
+                                             num_workers=args["workers"])
 
     y_true = []
     y_pred = []
@@ -267,7 +189,7 @@ def main(args):
             val_outputs = model(images)
             _, val_preds = torch.max(val_outputs, 1)
             val_correct += torch.sum(val_preds == labels.data)
-                
+
             y_true.extend(labels.detach().cpu().numpy().tolist())
             y_pred.extend(val_preds.detach().cpu().numpy().tolist())
 
@@ -287,29 +209,43 @@ def main(args):
     print(classificationReport)
 
     # Scriviamo i risultati in un file testuale
-    f = open("result/{}/{}/{}/res_validation_{}_{}_{}.txt".format(args["dataset"], args["attention"], args["gender"], args["attention"], args["dataset"], args["gender"]), "w")
-    
+    f = open("result/{}/{}/{}/res_validation_{}_{}_{}.txt".format(args["dataset"], args["attention"], args["gender"],
+                                                                  args["attention"], args["dataset"], args["gender"]),
+             "w")
+
     f.write(write)
     f.write(delimiter)
     f.write("Modello utilizzato: {}".format(args['loadModel']))
     f.write(delimiter)
     f.write("val_correct: {val_correct} || val_samples: {val_samples}".format(
-        val_correct = val_correct,
-        val_samples = len(val_data)))
-    f.write(delimiter)           
+        val_correct=val_correct,
+        val_samples=len(val_data)))
+    f.write(delimiter)
     f.write(classificationReport)
     f.close()
 
-    show_confusion_matrix(y_true, y_pred, labels_list, "result/{}/{}/{}/".format(args["dataset"], args["attention"], args["gender"]))
+    show_confusion_matrix(y_true, y_pred, labels_list,
+                          "result/{}/{}/{}/".format(args["dataset"], args["attention"], args["gender"]))
 
     if platform.system() == "Linux" and args['uses_drive']:
-        shutil.copy("result/{}/{}/{}/res_validation_{}_{}_{}.txt".format(args["dataset"], args["attention"], args["gender"], args["attention"], args["dataset"], args["gender"]), "../../gdrive/MyDrive/SysAg2022/{}/{}/{}/res_validation_{}_{}_{}.txt".format(args["dataset"], args["attention"], args["gender"], args["attention"], args["dataset"], args["gender"]))
-        shutil.copy("result/{}/{}/{}/confusion_matrix.png".format(args["dataset"], args["attention"], args["gender"]), "../../gdrive/MyDrive/SysAg2022/{}/{}/{}/confusion_matrix.png".format(args["dataset"], args["attention"], args["gender"]))
+        shutil.copy(
+            "result/{}/{}/{}/res_validation_{}_{}_{}.txt".format(args["dataset"], args["attention"], args["gender"],
+                                                                 args["attention"], args["dataset"], args["gender"]),
+            "../../gdrive/MyDrive/SysAg2022/{}/{}/{}/res_validation_{}_{}_{}.txt".format(args["dataset"],
+                                                                                         args["attention"],
+                                                                                         args["gender"],
+                                                                                         args["attention"],
+                                                                                         args["dataset"],
+                                                                                         args["gender"]))
+        shutil.copy("result/{}/{}/{}/confusion_matrix.png".format(args["dataset"], args["attention"], args["gender"]),
+                    "../../gdrive/MyDrive/SysAg2022/{}/{}/{}/confusion_matrix.png".format(args["dataset"],
+                                                                                          args["attention"],
+                                                                                          args["gender"]))
 
     print("===================================Testing Finished===================================")
 
+
 if __name__ == '__main__':
-      
     tuner_params = nni.get_next_parameter()
     logger.debug(tuner_params)
     params = vars(merge_parameter(return_args, tuner_params))
